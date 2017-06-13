226817bcff13e2e4bc97f74bcdf9b06e50086fa8
Lilium Wu <lilium.wu@yale.edu>
2017-06-13 14:17:03 -0700

    gets confused cuz don't know node

--- filesnested.js
+++ filesnested.js
@@ -1,8 +1,15 @@
 /*
    Goal of this exercise is to learn nested callbacks.
+   What you will be creating is a never-ending cycle of callbacks.
+   You have three text files, and js file.
+   You need to read data from all three files(remember fs!) and write to file no 4.
+   Then you will be reading data from file number 4, and writing to file number.
+   Welcome to Asynchronous world...
+
 */
 
 //require fs
+const fs = require('fs');
 
 
 //read file1
